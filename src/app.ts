911591ee11cfc6c0e22d9e719897a1791ec56d85
dev-ramshasaleem <ramshasaleem25@gmail.com>
2026-07-11 17:30:42 +0500

    updates

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import expenseRoutes from "./routes/expense.routes";
+import { errorMiddleware } from "./middleware/error.middleware";
 
 
 dotenv.config()
@@ -15,10 +17,13 @@ app.use(cors())
 app.use(helmet())
 app.use(morgan("dev"))
 app.use(express.json())
+app.use("/api/expenses", expenseRoutes)
+app.use(errorMiddleware)
 
 app.get('/', (req, res)=>{
     res.send("Expense Tracker API")
 })
+
 
 
 export default app
