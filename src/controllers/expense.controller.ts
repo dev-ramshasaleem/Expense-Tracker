1fc8f7f66bc24f9821a70e4c60d777c35dadf1d2
dev-ramshasaleem <ramshasaleem25@gmail.com>
2026-07-15 16:54:04 +0500

    new

--- src/controllers/expense.controller.ts
+++ src/controllers/expense.controller.ts
@@ -1,6 +1,85 @@
 import { Request, Response } from "express";
+import prisma from "../config/prisma.js";
 
-export const getExpenses = (
-    req: Request, res: Response) => {
-        res.json({message: "Get Expenses"})
+ 
+
+export const createExpense = async (
+  req: Request,
+  res: Response
+) => {
+
+  try {
+if (!req.user) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
     }
+    const userId = req.user.id;
+
+    const {
+  title,
+  amount,
+  category,
+  description,
+  date,
+} = req.body;
+
+const expense = await prisma.expense.create({
+  data: {
+    title,
+    amount,
+    category,
+    description,
+    date: new Date(date),
+    userId,
+  },
+});
+
+
+    res.status(201).json({
+      message:"Expense created successfully",
+      expense
+    });
+
+
+  } catch(error){
+     console.error("Create Expense Error:", error);
+
+    res.status(500).json({
+      message:"Server error"
+    });
+
+  }
+
+};
+export const getExpenses = async (req: Request, res: Response) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const expenses = await prisma.expense.findMany({
+      where: {
+        userId: req.user.id,
+      },
+      orderBy: {
+        date: "desc",
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      count: expenses.length,
+      expenses,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
--- src/routes/expense.routes.ts
+++ src/routes/expense.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import { createExpense, getExpenses } from "../controllers/expense.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get('/',(req,res)=>{
-    res.json({message: "Expense Routes"})
-})
+router.get("/", authMiddleware, getExpenses);
+router.post(
+ "/",
+authMiddleware,
+ createExpense
+);
 
 
 export default router;
