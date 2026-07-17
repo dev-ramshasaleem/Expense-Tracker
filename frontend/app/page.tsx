2bc4e81575fd4dbd7124508340c0e4836fdaa2e6
dev-ramshasaleem <ramshasaleem25@gmail.com>
2026-07-17 17:41:19 +0500

    yu

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-       <Image src="/background.jpg" alt="background image" width={100} height={20} priority></Image>
+    <div className="flex flex-col items-center justify-between min-h-screen">
+      <main className="flex  w-full max-w-3xl flex-col items-center justify-between ">
+       <Image src="/background.jpg" alt="background image" width={100} height={20} className="w-full h-full object-cover"></Image>
       </main>
     </div>
   );
