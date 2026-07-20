0bc520d2d0d42f7d2a9894e5e3c5e7152880ecbf
dev-ramshasaleem <ramshasaleem25@gmail.com>
2026-07-20 17:36:29 +0500

    new

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,8 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-console.log("PORT:", process.env.PORT);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
