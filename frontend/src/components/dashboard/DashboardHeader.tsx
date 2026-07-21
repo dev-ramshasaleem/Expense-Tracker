5439fb0129b9da73b562921dd92ece40c3341fb3
dev-ramshasaleem <ramshasaleem25@gmail.com>
2026-07-21 16:32:33 +0500

    new

--- frontend/src/components/dashboard/DashboardHeader.tsx
+++ frontend/src/components/dashboard/DashboardHeader.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { Plus, CalendarDays, Sparkles } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { CalendarDays, Plus, Sparkles } from "lucide-react";
 
 export default function DashboardHeader() {
+  // Replace this with the logged-in user's name later
+  const userName = "Ramsha";
+
+  const hour = new Date().getHours();
+
+  const greeting =
+    hour < 12 ? "Good Morning" : hour < 17 ? "Good Afternoon" : "Good Evening";
+
   const today = new Intl.DateTimeFormat("en-US", {
     weekday: "long",
     month: "long",
@@ -12,33 +20,22 @@ export default function DashboardHeader() {
   }).format(new Date());
 
   return (
-    <section className="flex flex-col gap-6 md:flex-row md:items-center md:justify-between">
-      {/* Left Side */}
+    <section className="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between">
+      {/* Left */}
       <div>
-        <h1 className="text-3xl font-bold tracking-tight text-white">
-          Good Morning, Ramsha
-          <Sparkles className="absolute right-240  h-7 w-7 -translate-y-8 text-white" />
+        <h1 className="text-3xl font-bold text-white md:text-4xl">
+          {greeting}, {userName} ✨
+          {/* <Sparkles className="absolute right-14  h-7 w-7 -translate-y-8 text-white" /> */}
         </h1>
 
-        <div className="mt-2 flex items-center gap-2 text-sm text-white">
+        <div className="mt-3 flex items-center gap-2 text-sm text-white">
           <CalendarDays className="h-4 w-4" />
           <span>{today}</span>
         </div>
       </div>
 
-      {/* Right Side */}
-      <Button
-        className="
-        w-full
-        md:w-auto
-        rounded-xl
-        bg-purple-600
-        px-6
-        py-6
-        text-white
-        hover:bg-purple-400
-        "
-      >
+      {/* Right */}
+      <Button className="h-12 w-full rounded-xl bg-cyan-500 px-6 text-white hover:bg-cyan-400 lg:w-auto">
         <Plus className="mr-2 h-5 w-5" />
         Add Expense
       </Button>
